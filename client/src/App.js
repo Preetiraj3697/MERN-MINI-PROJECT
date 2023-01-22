f9c311845bd9ff1c7245302cabd15e63b243de76
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-22 20:23:54 +0530

    add

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
-import Navbar from './components/Navbar';
 
+
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.css';
 const Navbar = () => {
   return (
-    <div>
-      <h1>Hello my navbar</h1>
+    <>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary " data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">MernMiniProject</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Registration</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
+    </>
   )
 }
 
