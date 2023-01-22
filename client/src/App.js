67a6fa856dbb093a4d896f326bb34b2a806451cf
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-22 20:41:55 +0530

    add

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
-
+import Home from './components/Home'
 import Navbar from './components/Navbar';
+import About from './components/About'
+import Contact from './components/Contact'
+import Login from './components/Login'
+import Signup from './components/Signup'
 function App() {
   return (
     <>
-      <Navbar />
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+      
     </>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
+import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary " data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">MernMiniProject</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary " data-bs-theme="dark">
+  <div className="container-fluid">
+    <NavLink className="navbar-brand" to="/">MernMiniProject</NavLink>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item active">
+        <NavLink className="nav-link" to="/">Home</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/about">About</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/contact">Contact</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/login">Login</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Registration</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/signup">Registration</NavLink>
         </li>
       </ul>
     </div>
